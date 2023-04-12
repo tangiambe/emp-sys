bf15a3094e3834e3056914dcd41e26b4233e4f91
Ahnaf Jabir Mir <jabirmir@gmail.com>
2023-04-12 15:15:10 -0400

    Created Deparment class, removed department refs

---
+++ department.py
@@ -0,0 +1,36 @@
+import employee
+
+class Department:
+    employees = []
+
+    def __init__(self, dept_name: str, dept_code: str, dept_contact_number: str, dept_budget: int, dept_head: employee) -> None:
+        self.dept_name = dept_name
+        self.dept_code = dept_code
+        self.dept_contact = dept_contact_number
+        self.budget = dept_budget
+        self.dept_head = dept_head
+        self.employees.append(dept_head)
+
+    def __str__(self) -> str:
+        return f"Name: {self.dept_name}\nCode: {self.dept_code}\nContact Number: {self.dept_contact}\nBudget: {self.budget}\nCurrent Head: {self.dept_head}"
+    def get_name(self) -> str:
+        return self.dept_name
+    def get_code(self) -> str:
+        return self.dept_code
+    def get_contact(self) -> str:
+        return self.dept_contact
+    def get_budget(self) -> str:
+        return self.budget
+    def get_dept_head(self) -> employee:
+        return self.dept_head
+    
+    def set_dept_name(self, name: str):
+        self.dept_name = name
+    def set_dept_code(self, code: str):
+        self.dept_code = code
+    def set_contact(self, contact: str):
+        self.dept_contact = contact
+    def set_budget(self, budget: int):
+        self.budget = budget
+    def set_dept_head(self, head: employee):
+        self.dept_head = head
--- employee.py
+++ employee.py
@@ -2,17 +2,16 @@
 
 class Employee:
 
-    def __init__(self, first_name: str, last_name: str, doe: str, salary: int, department: str) -> None:
+    def __init__(self, first_name: str, last_name: str, doe: str, salary: int) -> None:
         self.first_name = first_name.capitalize()
         self.last_name = last_name.capitalize()
         self.doe = doe
         self.salary = salary
-        self.department = department
         self.id = create_company_id(self.first_name, self.last_name, self.doe)
         self.email = create_company_email(self.first_name, self.last_name, self.doe[2:4])
     
     def __str__(self) -> str:
-        return f"{self.first_name} {self.last_name}, {self.doe}, {self.salary}, {self.department}, {self.id}, {self.email}"
+        return f"First Name: {self.first_name}\nLast Name: {self.last_name}\nDate of Employment: {self.doe}\nSalary: {self.salary}\nID: {self.id}\nEmail: {self.email}"
     
     def get_email(self) -> str:
         return self.email
@@ -55,11 +54,11 @@ class Employee:
 
 def create_company_email(first_name: str, last_name: str, doe: str) -> str:
     email = ''
-    email = email + first_name.lower() + '.' + last_name.lower() + employment_year[2:4] + "@company.com"
+    email = email + first_name.lower() + '.' + last_name.lower() + doe[2:4] + "@company.com"
     return email
 
 def create_company_id(first_name: str, last_name: str, doe: str) -> str:
     initials = first_name.lower()[0] + last_name.lower()[0]
-    year_joined = year[2:4]
+    year_joined = doe[2:4]
 
     return initials + year_joined
