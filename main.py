93096113dd94c52cc20a99d77e42af34ff313d61
tangiambe <59839859+tangiambe@users.noreply.github.com>
2023-04-12 14:16:40 -0400

    Added main menu

---
+++ main.py
@@ -0,0 +1,52 @@
+import sys
+from employee import Employee
+from employee import create_company_id
+
+
+emp_list = []
+
+def menu_choices():
+    print("\n************* Employee Management System ***************")
+    print("1) Add Employee          2) Remove Employee")
+    print("3) Update Employee       4) List Employee Information")
+    print("0) Log Out")
+
+def exitCase(exit_key: str):
+    if input(f"\nEnter any key to return to Menu. ({exit_key} to Log Out): ") == exit_key:
+        print("Logging Out.....Bye Bye!")
+        return True
+
+
+while True:
+    menu_choices()
+    option = input("\nPick an option: ")
+    
+    match option:
+        case '0':
+            print("Logging Out......Bye Bye!")
+            sys.exit()
+        case '1':
+            emp_fname = input("Enter first name: ")
+            emp_lname = input("Enter last name: ")
+            emp_doe = input ("Enter date of employment (YYYY MM DD): ")
+            emp_salary = int(input("Enter salary: "))
+            emp_dept = input ("Enter Employee's department: ")
+            new_emp = Employee(emp_fname, emp_lname, emp_doe, emp_salary, emp_dept)
+            emp_id = create_company_id(new_emp.get_first_name(), new_emp.get_last_name(), new_emp.get_doe())
+            emp_list.append(new_emp)
+            
+            if exitCase('0'):
+                sys.exit()
+            else: 
+                continue
+        case '4':
+            for emp in emp_list:
+                print(emp)
+                
+            if exitCase('0'):
+                sys.exit()
+            else: 
+                continue
+        case _:
+            print("~Error: Invalid Input! PLEASE ENTER A VALID OPTION.~\n")
+            
