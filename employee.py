cb32603baf7474501bdd5c9502f670e66708f7c8
tangiambe <tafor@yahoo.com>
2023-04-12 14:14:51 -0400

    main menu added

--- employee.py
+++ employee.py
@@ -1,4 +1,4 @@
-import yaml
+# import yaml
 
 list_of_employees = []
 set_of_emails = set()
@@ -11,11 +11,11 @@ class Employee:
         self.doe = doe
         self.salary = salary
         self.department = department
-        self.id = create_company_id(self.first_name, self.last_name, self.doe[0])
-        self.email = create_company_email(self.first_name, self.last_name, self.doe[0])
+        self.id = create_company_id(self.first_name, self.last_name, self.doe)
+        self.email = create_company_email(self.first_name, self.last_name, self.doe[2:4])
     
     def __str__(self) -> str:
-        return f"{self.first_name}, {self.last_name}, {self.doe}, {self.salary}, {self.department}, {self.id}, {self.email}"
+        return f"{self.first_name} {self.last_name}, {self.doe}, {self.salary}, {self.department}, {self.id}, {self.email}"
     
     def get_email(self) -> str:
         return self.email
@@ -58,11 +58,11 @@ class Employee:
 
 def create_company_email(first_name: str, last_name: str, employment_year: str) -> str:
     email = ''
-    email = email + first_name.lower() + last_name.lower() + employment_year[-2:] + "@company.com"
+    email = email + first_name.lower() + '.' + last_name.lower() + employment_year[2:4] + "@company.com"
     return email
 
 def create_company_id(first_name: str, last_name: str, year: str):
     initials = first_name.lower()[0] + last_name.lower()[0]
-    year_joined = year[-2:]
+    year_joined = year[2:4]
 
     return initials + year_joined
