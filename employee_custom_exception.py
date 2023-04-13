29018c245c2a23a49244e6cc29b446c4791bf33f
Ahnaf Jabir Mir <jabirmir@gmail.com>
2023-04-13 14:55:35 -0400

    created new custom exceptions

--- employee_custom_exception.py
+++ employee_custom_exception.py
@@ -1,7 +1,25 @@
 from employee import *
 
-class NoLastNameException(Exception):
+class NoNameException(Exception):
     def __init__(self, name):
         self.name = name
     def __str__(self):
-        return("Employee"+self.name+" does not have a last name!")
+        return("Employee" + self.name + " does not have an appropriate name!")
+    
+class LowBudgetException(Exception):
+    def __init__(self, budget):
+        self.budget = budget
+    def __str__(self):
+        return("Budget of"+ self.budget +" is too low to be assigned as a budget for a department!")
+    
+class LowSalaryException(Exception):
+    def __init__(self, salary):
+        self.salary = salary
+    def __str__(self):
+        return("Salary of"+ self.salary +" is too low to be assigned to an employee!")
+    
+class ContactNumberException(Exception):
+    def __init__(self, number):
+        self.number = number
+    def __str__(self):
+        return("The number given "+ self.number +" does not follow the correct format! Must be 123-4567")
